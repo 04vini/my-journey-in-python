bcb92e3d5975e5838573ffafd5589892c46d7823
Marcos Vinicius Hernandes Da Silva Carreiro <35454194+04vini@users.noreply.github.com>
2024-01-25 15:42:59 -0300

    Add files via upload

---
+++ Python estudo/exercicio 3.12.py
@@ -0,0 +1,6 @@
+distancia = int (input ("Digite a distancia percorrida: "))
+kmPH = int (input ("Qual a velocidade média? "))
+quilometrosPerH = distancia * kmPH/100
+
+
+print("A distancia para seu destino é: ", distancia," quilometros, e você está a ", kmPH, " quilometros por hora", " você irá chegar ao seu destino em: ", quilometrosPerH, " horas")
