58a09e7a3965c06884b7fd1129e2df14bc184637
Marcos Vinicius Hernandes Da Silva Carreiro <35454194+04vini@users.noreply.github.com>
2024-01-25 15:47:28 -0300

    Add files via upload

---
+++ Python estudo/exercicio 3.13.py
@@ -0,0 +1,4 @@
+temperaturaC = float (input("Digite a temperatura: "))
+temperaturaF = ((9 * temperaturaC) / 5) + 32
+
+print("Sua temperatura em celsios é: ", temperaturaC, " que convertida em fahrenheit é: ", temperaturaF)
